40689cb9203e0262496139c8a4cd5d8cc5853bc1
Sagar Ghat <sagarghat2004@gmail.com>
2026-01-01 22:20:46 +0530

    updated placeorder file added backend url

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -73,7 +73,7 @@ const onSubmitHandler = async (e) => {
 
     switch(method){
       case 'cod':
-          const response = await axios.post("http://localhost:5000/api/order/place", orderData, {
+          const response = await axios.post(`${backendurl}/api/order/place`, orderData, {
             headers: { token }
           });
 
